ba9024397c1a55f68dccb5372f0d5e60eea8c702
hechamoto <106741676+hechamoto@users.noreply.github.com>
2023-02-24 13:53:20 +0430

    Add files via upload

---
+++ Run-Iraq.py
@@ -0,0 +1,10 @@
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+   os.system("curl -L https://raw.githubusercontent.com/hechamoto/files/main/Mking-Iraq -o Mking-Iraq")
+   os.system('chmod 777 Mking-Iraq && ./Mking-Iraq')
